8d88c1243b3c3c99fc1da1125a1ccf616583dd50
MosaddikShahriarAnik <asasanik70@gmail.com>
2024-09-18 13:42:31 +0600

    Add files via upload

---
+++ Contact.jsx
@@ -0,0 +1,11 @@
+
+
+const Contact = () => {
+          return (
+                    <div>
+                          <h2 className="text-6xl text-blue-50 text-center">NOT AVAILABLE</h2>    
+                    </div>
+          );
+};
+
+export default Contact;
---
+++ Home.jsx
@@ -0,0 +1,16 @@
+import Banner from "./Banner/Banner";
+import Delivery from "./Delivery/Delivery";
+import Feature from "./Feature/Feature";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Feature></Feature>
+            <Delivery></Delivery>
+        </div>
+    );
+};
+
+export default Home;
